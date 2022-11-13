2514dc3fbe1426c02705fa0e9b8ec7d2a2211def
don-tnowe <67479453+don-tnowe@users.noreply.github.com>
2022-11-13 16:36:31 +0200

    Better example dict

--- average_dictionary_enjoyer.gd
+++ average_dictionary_enjoyer.gd
@@ -2,6 +2,6 @@ class_name AverageDictionaryEnjoyer
 extends Resource
 
 export var dict1 := {}
-export var dict2 := {"cool_property" : 9}
-export var dict3 := {"boring_property" : Color.gray}
+export var dict2 := {}
+export var dict3 := {}
 export var res : Resource
