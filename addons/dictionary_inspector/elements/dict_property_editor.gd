1b92b62b87ad201b16d6c7e6c8cfddbdf59636ab
don-tnowe <67479453+don-tnowe@users.noreply.github.com>
2022-11-15 10:38:13 +0200

    Implement DICTIONARY reordering

--- addons/dictionary_inspector/elements/dict_property_editor.gd
+++ addons/dictionary_inspector/elements/dict_property_editor.gd
@@ -6,10 +6,12 @@ var last_type_k := TYPE_STRING
 
 
 func add_all_properties(collection):
+	var i := 0
 	for k in collection:
 		last_type_v = typeof(collection[k])
 		last_type_k = typeof(k)
-		add_child(create_property_container(k))
+		add_child(create_property_container(k, i))
+		i += 1
 
 
 func display_key_on_label(value, label):
@@ -22,8 +24,11 @@ func display_key_on_label(value, label):
 		label.self_modulate = Color.white
 
 
-func create_property_container(k):
+func create_property_container(k, i = 0):
 	var c = init_prop_container.duplicate()
+	c.add_child(EditorArrayIndex.new(i))
+	c.get_child(0).connect("drop_received", self, "_on_item_moved", [i])
+
 	var label = Label.new()
 	display_key_on_label(k, label)
 
@@ -43,27 +48,36 @@ func create_property_container(k):
 
 func toggle_property_editable(k, container):
 	var children = container.get_children()
-	if children[0].visible:
-		children[0].hide()
+	if children[1].visible:
+		children[1].hide()
 		container.add_child(create_type_switcher(typeof(k), k, true))
 		container.add_child(create_property_control_for_type(typeof(k), k, k, true))
 		container.add_child(create_type_switcher(typeof(stored_collection[k]), k, false))
-		container.move_child(children[1], 4)
-		container.move_child(children[2], 6)
+
+		# Move button and value editor to front
+		container.move_child(children[2], 5)
+		container.move_child(children[3], 6)
 
 	else:
-		display_key_on_label(k, children[0])
-		children[0].show()
-		children[1].queue_free()
+		display_key_on_label(k, children[1])
+		children[1].show()
 		children[2].queue_free()
-		children[4].queue_free()
+		children[3].queue_free()
+		children[5].queue_free()
 
 
 func update_variant(key, value, is_rename = false):
 	if is_rename:
 		if (typeof(value) != typeof(key) || value != key):
+			var new_collection = {}
+			var old_keys = stored_collection.keys()
+			var found_at_index = old_keys.find(key)
+			old_keys[found_at_index] = value
 			stored_collection[value] = stored_collection[key]
-			stored_collection.erase(key)
+			for x in old_keys:
+				new_collection[x] = stored_collection[x]
+
+			stored_collection = new_collection
 	
 	else:
 		stored_collection[key] = value
@@ -110,14 +124,17 @@ func _on_property_control_value_changed(value, control, key, is_rename = false):
 	var old_value = stored_collection[key]
 	update_variant(key, value, is_rename)
 	connect_control(control, typeof(value), value, true)
-	connect_control(parent_children[5], typeof(old_value), value, false)
+	connect_control(parent_children[6], typeof(old_value), value, false)
 	yield(get_tree(), "idle_frame")
-	parent_children[1].replace_by(create_type_switcher(typeof(value), value, true))
-	parent_children[1].free()
-	parent_children[3].disconnect("pressed", self, "toggle_property_editable")
-	parent_children[3].connect("pressed", self, "toggle_property_editable", [value, control.get_parent()])
-	parent_children[4].replace_by(create_type_switcher(typeof(old_value), value, false))
-	parent_children[4].free()
+
+	# TODO: fix this mess by binding the reference when connecting,
+	# then just grabbing the ref's index in parent
+	parent_children[2].replace_by(create_type_switcher(typeof(value), value, true))
+	parent_children[2].free()
+	parent_children[4].disconnect("pressed", self, "toggle_property_editable")
+	parent_children[4].connect("pressed", self, "toggle_property_editable", [value, control.get_parent()])
+	parent_children[5].replace_by(create_type_switcher(typeof(old_value), value, false))
+	parent_children[5].free()
 
 
 func _on_property_control_type_changed(type, control, key, is_key = false):
@@ -128,10 +145,12 @@ func _on_property_control_type_changed(type, control, key, is_key = false):
 	var value = default_per_class[type]
 	update_variant(key, value, is_key)
 	var old_prop = control.get_parent()
-	add_child_below_node(old_prop, create_property_container(value if is_key else key))
+	var new_prop = create_property_container(value if is_key else key)
+	add_child_below_node(old_prop, new_prop)
 	old_prop.free()
 	if is_key:
 		last_type_k = type
+		call_deferred("toggle_property_editable", value, new_prop)
 
 	else:
 		last_type_v = type
@@ -141,3 +160,15 @@ func _on_property_deleted(key, control):
 	stored_collection.erase(key)
 	control.get_parent().queue_free()
 	emit_signal("value_changed", stored_collection)
+
+
+func _on_item_moved(from, to):
+	var old_keys = stored_collection.keys()
+	old_keys.insert(to, old_keys.pop_at(from))
+
+	var new_collection = {}
+	for x in old_keys:
+		new_collection[x] = stored_collection[x]
+	
+	display(new_collection, plugin)
+	emit_signal("value_changed", new_collection)
